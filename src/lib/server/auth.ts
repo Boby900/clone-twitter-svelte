9ac6098ff29306b3283645babb2df25834bc9f7f
Boby900 <bobytiwari310@gmail.com>
2024-07-29 19:32:23 +0530

    refactor: Reorder imports in auth.ts for better readability

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,7 +1,8 @@
 import { Lucia } from "lucia";
 import { dev } from "$app/environment";
 import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
-import { db, sessionTable, userTable } from "./db";
+import {  sessionTable, userTable } from "./schema";
+import {  db } from "./db";
 const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
 export const lucia = new Lucia(adapter, {
 	sessionCookie: {
