24aacefbf20d64de50d9ee68523776496e9d4bea
Boby900 <bobytiwari310@gmail.com>
2024-07-30 20:09:28 +0530

    refactor: Update login page layout and styles

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,13 +6,18 @@
 	export let form: ActionData;
 </script>
 
-<h1>Sign in</h1>
-<form method="post" use:enhance>
-	<label for="username">Username</label>
-	<input name="username" id="username" /><br />
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password" /><br />
-	<button>Continue</button>
-	<p>{form?.message ?? ""}</p>
-</form>
-<a href="/signup">Create an account</a>
+<div class="flex  justify-center m-2 p-2 text-2xl ">
+	<div>
+		<h1>Sign in</h1>
+		<form class="p-2 m-2" method="post" use:enhance>
+			<label  for="username">Username</label>
+			<input name="username" id="username" /><br />
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password" /><br />
+			<button>Continue</button>
+			<p>{form?.message ?? ""}</p>
+		</form>
+		<a href="/signup">Create an account</a>
+	</div>
+	
+</div>
