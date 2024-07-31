ef8e46d8c1d916c6b50af842c61a5ac66005edd7
Boby900 <bobytiwari310@gmail.com>
2024-07-31 19:20:16 +0530

    feat: Add form data handling to default action in post page

--- src/routes/post/+page.server.ts
+++ src/routes/post/+page.server.ts
@@ -1,6 +1,9 @@
 import type { Actions } from './$types';
 export const actions: Actions ={
-    default: async () => {
+    default: async (event) => {
+        const formData = await event.request.formData();
+        const content = formData.get('content');
+        console.log(content)
         console.log("this shit is working!")
     }
 }
