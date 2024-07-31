d2fe8dad49e5faeaf4c4f5dbba2336784f09772d
Boby900 <bobytiwari310@gmail.com>
2024-07-31 18:20:51 +0530

    refactor: Remove navigation bar from home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,7 @@
     import { ModeWatcher } from "mode-watcher";
    
   </script>
-  <nav  >
-    
-    <ul class="text-2xl h-[100px] bg-zinc-500 align-middle pt-8  justify-around flex">
-   
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-    </ul>
-</nav>
+  
 
     <ModeWatcher />
   <slot />
