5ce7e78efd92e767dc5387bb1419bec387be1315
Boby900 <bobytiwari310@gmail.com>
2024-07-31 18:03:01 +0530

    feat: Improve login form layout and styles

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -8,13 +8,13 @@
 
 <div class="flex  justify-center m-2 p-2 text-2xl ">
 	<div>
-		<h1>Sign in</h1>
+		
 		<form class="p-2 m-2" method="post" use:enhance>
-			<label  for="username">Username</label>
-			<input name="username" id="username" /><br />
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password" /><br />
-			<button>Continue</button>
+		
+			<input name="username" placeholder="username" id="username" /><br />
+
+			<input type="password" placeholder="password" name="password" id="password" /><br />
+			<button class="pt-4">Continue</button>
 			<p>{form?.message ?? ""}</p>
 		</form>
 		<a href="/signup">Create an account</a>
