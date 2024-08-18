4581ee8955fe45c9764abebd7c463176bd8740c1
Boby900 <bobytiwari310@gmail.com>
2024-08-18 19:23:16 +0530

    refactor: Add GitHub avatar URL to user table

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -4,7 +4,8 @@ export const userTable = pgTable('user', {
 	id: text('id').primaryKey(),
 	username: text('username'),
 	password_hash: text('password_hash'),
-	github_id: integer('github_id').unique()
+	github_id: integer('github_id').unique(),
+	github_avatar_url: text('github_avatar_url'),
 });
 
 export const sessionTable = pgTable('session', {
@@ -28,4 +29,5 @@ export interface DatabaseUser {
 	id: string;
 	username: string;
 	password_hash: string;
+	avatar_url: string;
 }
