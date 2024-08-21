ca0b8f755a44ce11483dc2a2d8b97614a7c25a1a
Boby900 <bobytiwari310@gmail.com>
2024-08-21 18:03:31 +0530

    refactor: Update UI styles and remove unused code

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,11 +1,18 @@
 import type { PageServerLoad } from './$types';
+import { userTable } from '$lib/server/schema';
+import { db } from '$lib/server/db';
+import { isNotNull } from 'drizzle-orm';
+import { redirect } from '@sveltejs/kit';
+
 
 export const load: PageServerLoad = async (event) => {
 	if (!event.locals.user) {
-		return "this is bob"
+		return redirect(302, '/');
 	}
+	const users = await db.select().from(userTable).where(isNotNull(userTable.github_avatar_url));
 	return {
 		
-		user: event.locals.user
+		user: event.locals.user,
+		users
 	};
 };
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,36 +1,21 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import bob from '$lib/assets/icons8-x-64.png';
 	export let data: PageData;
-	let imageUrl: string = '';
-   let name: string = '';
-	async function fetchData() {
-		const response = await fetch(`/api`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		});
-		const data = await response.json();
-		if (data[0].github_avatar_url && data[0].username !== '') {
-			imageUrl = data[0].github_avatar_url;
-         name = `Hii, ${data[0].username}`;
-		}
-		console.log(data);
-	}
+	
+   console.log(data);
+	
 </script>
 
 <div class="flex flex-col items-center justify-center">
 	<div class="p-4 m-4">
 		<img
-			src={imageUrl}
+			src={data.users[0].github_avatar_url}
 			class="rounded-full h-[200px] w-[200px] border border-rose-30"
 			alt="Your avatar"
 		/>
 	</div>
 	<div></div>
 	<div class="p-4 m-4">
-		<p> {name}</p>
-		<button on:click={fetchData} class="bg-rose-500 text-white p-2 rounded-md">Edit Profile</button>
+		<p> {`Hi, ${data.users[0].username}`}</p>
 	</div>
 </div>
